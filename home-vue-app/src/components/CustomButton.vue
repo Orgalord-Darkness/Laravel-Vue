0078524b97554295b58b515eff02d4be4099e4ea
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2025-03-23 08:33:40 +0100

    Création de routes et de navigations

---
+++ home-vue-app/src/components/CustomButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <button class="button">{{ text }}</button>
+</template>
+
+<script>
+export default {
+    name: 'CustomButton',
+    props: ['text']
+}
+</script>
+
+<style>
+    .button {
+        background-color: blue;
+        color: white;
+        border-radius: 10px;
+    }
+</style>
---
+++ home-vue-app/src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Nous vous proposons de découvrir de nombreux personnages de fictions.</h1>
+  </div>
+</template>
